0284fa4191bce5fca89285c1505a0d71f8aa5ef8
Lindsey Fowler <lindsey.fowler@cenveo.com>
2017-05-24 15:10:11 -0400

    Add homepage version 1 with styling and images

---
+++ src/js/scripts.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() { 
+    $('.carousel').slick(
+            dots: true,
+                autoplay: true,
+                autoplaySpeed: 3500,
+                infinite: true,
+                fade: true,
+                pauseOnHover: true, 
+                slidesToScroll: 1
+    );
+});
+      
